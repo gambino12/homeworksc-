d44633a6ba84b8770ad7b6eab2d22a520669bd75
sanya.mef <lightsan52rus@yandex.ru>
2022-04-17 16:29:13 +0300

    homeworks is done))))

---
+++ homework 1/Program.cs
@@ -0,0 +1,71 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("iput first intenger number: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("iput second intenger number: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("max: " + num1);
+    Console.WriteLine("min: " + num2);
+}
+else
+{
+    Console.WriteLine("min: " + num1);
+    Console.WriteLine("max: " + num2);
+}
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("iput first intenger number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("iput second intenger number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("iput third intenger number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 & num1 > num3 )
+{
+   Console.Write("max = " + num1) ;
+}
+if (num2 > num1 & num2 > num3 )
+{
+   Console.Write("max = " + num2);
+}
+if (num3 > num1 & num3 > num2 )
+{
+   Console.Write("max = " + num3);
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("iput  intenger number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+  Console.Write(num + " - an even number");  
+}
+else
+{
+    Console.Write(num + " - odd number");
+}
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("input  intenger number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while (current <= num)
+{
+  if (current % 2 == 0)
+  {
+   Console.Write(current + " ");
+   current++;
+  }
+  else
+    current++;
+}
+*/
