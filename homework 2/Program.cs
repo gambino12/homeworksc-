6ec830e6789d2ec0f22146243c4c799b07db0327
sanya.mef <lightsan52rus@yandex.ru>
2022-04-19 19:00:52 +0300

    сделал дз 2

---
+++ homework 2/Program.cs
@@ -0,0 +1,60 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void ShowNumber()
+{
+    int n = new Random().Next(100,999);
+    Console.Write(n + " -> ");
+    int n3 = n/10%10;
+    Console.Write(n3);
+    
+}
+
+ShowNumber();
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void ShowNumber()
+{
+    int n = new Random().Next();
+    Console.Write(n + " -> ");
+    if (n < 100)
+    {
+        Console.Write("no third digit");
+    }
+    else
+   {
+    int n3 = n/100;
+    int n4 = n%10;
+    int n5 = я очень долго думал что тут делать, но я очень устал;
+    Console.Write(тут что то должго быть но я тупой);
+   }  
+   
+}
+
+ShowNumber();
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+
+
+void ShowNumber()
+{
+   Console.Write("write the day of the week as a number -> ");
+    int dayNumber = Convert.ToInt32(Console.ReadLine());
+   if(dayNumber > 7)
+   {
+       Console.WriteLine("rest buddy :3");
+       Console.Write("only 7 days a week");
+   }
+   else{
+   if (dayNumber > 5)
+   {
+       Console.Write("cheers weekend :D");
+   }
+else
+{
+    Console.Write("not a day off :(");
+} }
+}
+ShowNumber();
+*/
