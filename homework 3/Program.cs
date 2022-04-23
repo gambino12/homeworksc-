8b05a293ccab71a432a9b91649407717b810d91b
sanya.mef <lightsan52rus@yandex.ru>
2022-04-23 17:04:56 +0300

    3 hw done :D

---
+++ homework 3/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+void palindrome(int num)
+{
+      if(num < 10000 || num > 99999)
+{
+Console.Write("Invalid number");
+return;
+}
+    if (num / 10000 == num%10 && num /1000%10 == num/10%10)
+        Console.Write("omg it's palindrome");
+    else 
+        Console.Write("sorry bro it's not a palindrome");
+
+}
+Console.Write("input five-digit number -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+palindrome(num);
+
+double Lenght(double ax ,double ay,double az,double bx,double by,double bz)
+{
+ return Math.Sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay)+(bz*az)*(bz*az));
+}
+
+Console.Write("input a(x) coordinate -> ");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a(y) coordinate -> ");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a(z) coordinate -> ");
+int az = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b(x) coordinate -> ");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b(y) coordinate -> ");
+int by = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b(z) coordinate -> ");
+int bz = Convert.ToInt32(Console.ReadLine());
+Console.Write(Lenght(ax,ay,az,bx,by,bz));
+
+void quad(int x)
+{
+int index = 1;
+int quadnumber = 1;
+ while (index <= x)
+  {  
+    quadnumber = index * index * index;
+    Console.Write(quadnumber + " ");
+    index++;
+  }
+  
+  
+}
+Console.Write("input number -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+quad(number);
+*/
+//ничего себя запотел,даже чувствую себя умным
